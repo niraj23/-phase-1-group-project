89956e4d5049f79ec47fa21764b253f10bc756a9
Niraj <niraj2323@gmail.com>
2021-10-26 13:01:16 -0400

    Issue with the css and card size

--- index.js
+++ index.js
@@ -22,16 +22,65 @@ const makeTeamTiles = teamObj => {
     teamLogo.title = `${teamObj.teamName}`
 
     teamTile.appendChild(teamLogo)
-
-//Add functionality to team tile
-    //Passing team id when clicked
-    teamTile.addEventListener('click', () => {
-        teamContainer.style.display = "None"
-        document.getElementById('player-container').style.display = "flex"
-    })
-        //callback function to be provided by Niraj
-
-    //Append tile to team container
     teamContainer.appendChild(teamTile)
 
+    
+    teamTile.addEventListener('click', () => {
+        teamContainer.style.display = "None"
+        let playerContainer  = document.getElementById('player-container')
+        playerContainer.style.display = "flex";
+        
+        teamObj.players.forEach(function(players) {
+            const card = document.createElement("div");
+            const backInfo = document.createElement("h4")
+            const cardBack = document.createElement("div")
+            const cardInner = document.createElement("div");
+            const cardFront = document.createElement("div");
+            const cardImage = document.createElement("img");
+            const cardName = document.createElement("h3");
+            const likeBttn = document.createElement("button");
+
+            cardInner.className = "sports-card-inner";
+            cardFront.className = "card__face card__face--front";
+            backInfo.className = "back-info";
+            cardBack.className = "card__face card__face--back";
+        
+            card.id = `${players.idPlayer}`;
+            card.className = "sports-card";
+            card.style.borderColor = players.primary;
+
+            card.onmouseover = function() {
+            var colorString = '0px 8px 16px 0px ' + players.secondary;
+            this.style['box-shadow'] = colorString;
+            this.style['-webkit-box-shadow'] = colorString;
+            this.style['-moz-box-shadow'] = colorString;
+            }  
+            card.onmouseout = function() {
+            this.style['box-shadow'] = "none";
+            this.style['-webkit-box-shadow'] = "none";
+            this.style['-moz-box-shadow'] = "none";
+            }
+            
+            cardImage.src = players.player_image;
+            cardImage.className = 'card-image';
+            cardName.textContent = players.espn_player_name;
+        
+            likeBttn.className = "like-bttn";
+            likeBttn.textContent = "♥";
+
+            backInfo.textContent = "Hey if you see this you got the card to flip";
+        
+            cardFront.append(cardImage, cardName, likeBttn);
+            cardBack.append(backInfo);
+            cardInner.appendChild(cardFront, cardBack);
+            card.appendChild(cardInner);
+            playerContainer.appendChild(card);
+        
+    
+    cardInner.addEventListener("click", function () {
+        cardInner.classList.toggle('is-flipped');
+    })
+
+});
+})
 }
