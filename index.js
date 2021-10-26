adf4d5c06f305c7d721bc161bd3ed629bbb6960e
Daniel <dandesantis14@gmail.com>
2021-10-26 18:35:25 -0400

    return button functionality, fixed broken teams-container

--- index.js
+++ index.js
@@ -1,24 +1,43 @@
 window.addEventListener('DOMContentLoaded', () => init())
 
-const init = () => getTeams().then(teams => teams.forEach(el => makeTeamTiles(el)))
+const init = () => {
+    getTeams().then(teams => teams.forEach(el => makeTeamTiles(el)))
+    teamSelectBttnEvent()
+}
 
 const getTeams = () => {
-    return fetch('http://localhost:3000/teams').then(resp=>resp.json())
+    return fetch('http://localhost:3000/teams').then(resp => resp.json())
 }
-//Declare variables that are consistently referenced
-const teamSelectBttn = document.getElementById('team-page-return')
-const teamContainer = document.getElementById('team-container')
-const playerContainer = document.getElementById('player-container')
+//set up button functionality
+function teamSelectBttnEvent() {
+    const teamSelectBttn = document.getElementById('team-page-return')
+    const teamContainer = document.getElementById('team-container')
+    const playerContainer = document.getElementById('player-container')
+    teamSelectBttn.addEventListener('click', () => {
+        hider(playerContainer)
+        teamContainer.style.display = "flex"
+        teamSelectBttn.style.display = "none"
+        deletePlayers()
+    })
+}
 
-teamSelectBttn.addEventListener('click', () =>{
-    hider(playerContainer)
-    teamContainer.style.display = "flex" 
-    deletePlayers()
-})
+//Delete function to remove player cards from DOM
+const deletePlayers = () => {
+    const allPlayers = document.querySelectorAll('.sports-card-inner')
+    allPlayers.forEach(el => el.remove())
+}
+
+//function to hide elements
+const hider = element => {
+    element.style.display = "none"
+}
+
 
 //Makes all the team tiles
 const makeTeamTiles = teamObj => {
     
+    const teamContainer = document.getElementById('team-container')
+
     const teamTile = document.createElement('div')
     teamTile.className = "team-card"
     teamTile.id = teamObj.id
@@ -34,20 +53,25 @@ const makeTeamTiles = teamObj => {
 
     teamTile.appendChild(teamLogo)
 
+//Append tile to team container
+    teamContainer.appendChild(teamTile)
+
 //Add functionality to team tile
     //Passing team id when clicked
     teamTile.addEventListener('click', e => {
+        const playerContainer = document.getElementById('player-container')
         //hide teams
         hider(teamContainer)
+        //grab team return button
+        const teamSelectBttn = document.getElementById('team-page-return')
         //display team selector return button
         teamSelectBttn.style.display = "block"
         //display playerscontainer
         playerContainer.style.display = "flex"
         //build player cards
-        nirajCallbackFunction(e.target.id)
+        // nirajCallbackFunction(e.target.id)
     })
-    //Append tile to team container
-    teamContainer.appendChild(teamTile)
+
 }
 
 //Makes all player cards
@@ -55,13 +79,4 @@ const nirajCallbackFunction = teamId => {
     console.log('something')
 }
 
-//Delete function to remove player cards from DOM
-const deletePlayers = () => {
-    const allPlayers = document.querySelectorAll('.sports-card-inner')
-    allPlayers.forEach(el => el.remove())
-}
 
-//function to hide elements
-const hider = element => {
-    element.style.display = "none"
-}
